7affc4ed6dcd03968c14a21f83f2114f1bb1e4b8
Juliano <judev.software@gmail.com>
2022-03-22 08:40:42 -0300

    Remove knex and add pg. Setup linter.

--- Api/src/shared/app/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var app = (0, express_1["default"])();
-app.use(express_1["default"].json());
-app.listen('3333', function () {
-    console.log('Server running on port 3333');
-});
--- Api/src/shared/database/connection.ts
+++ Api/src/shared/database/connection.ts
@@ -1,5 +1,5 @@
 import knex from 'knex';
-import path from 'path'
+import path from 'path';
 
 const database = knex({
   client: 'sqlite3',
