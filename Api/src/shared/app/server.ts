337145ca32e64e6714ec7c376fea2eaa7130da22
Juliano <judev.software@gmail.com>
2022-03-22 19:49:26 -0300

    Config cors.

--- Api/src/shared/app/server.ts
+++ Api/src/shared/app/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express from 'express';
+import cors from 'cors';
 
 import routes from './routes';
 
@@ -9,6 +10,7 @@ import '../container/dependecyInjector';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, () =>
